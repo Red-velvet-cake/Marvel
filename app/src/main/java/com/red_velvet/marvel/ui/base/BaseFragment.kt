edc24970f818f7b3a0f7d0ae2e521c094d548bea
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-28 20:13:23 +0300

    create base fragment

---
+++ app/src/main/java/com/red_velvet/marvel/ui/base/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.red_velvet.marvel.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.red_velvet.marvel.BR
+
+abstract class BaseFragment<VDB : ViewDataBinding> : Fragment() {
+    abstract val layoutIdFragment: Int
+    abstract val viewModel: BaseViewModel
+
+    private lateinit var _binding: VDB
+    protected val binding: VDB
+        get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutIdFragment, container, false)
+        _binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, viewModel)
+            return root
+        }
+    }
+
+}
