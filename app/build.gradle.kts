0b28f4dab4bdd5103aa2b790a19e2190a6dd914c
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-29 22:33:37 +0300

    add gson dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
     // OkHttp
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
