ac4d19d4ec472ba0e6a13a5b1c364e4496181e36
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-05-01 18:20:18 +0300

    add getAllSeries() fun in MarvelRepositoryImpl.kt class

--- app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepositoryImpl.kt
+++ app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.red_velvet.marvel.data.repository
 import com.red_velvet.marvel.data.model.ComicsResponse
 import com.red_velvet.marvel.data.model.BaseResponse
 import com.red_velvet.marvel.data.model.CharactersByEventIdResponse
+import com.red_velvet.marvel.data.model.GetAllSeries
 import com.red_velvet.marvel.data.remote.MarvelService
 import com.red_velvet.marvel.data.remote.RetrofitClient
 import io.reactivex.rxjava3.core.Single
@@ -31,7 +32,7 @@ class MarvelRepositoryImpl(
 
 
     //TODO Series(use **search starts with** and **contains** QP)
-
+    override fun getAllSeries() = RetrofitClient.apiService.getAllSeries()
 
     //TODO Serie details
 
@@ -43,7 +44,7 @@ class MarvelRepositoryImpl(
 
 
     //TODO Events(Characters by event id)
-    override fun getCharactersByEventId(eventId:Int): Single<BaseResponse<CharactersByEventIdResponse>> {
+    override fun getCharactersByEventId(eventId: Int): Single<BaseResponse<CharactersByEventIdResponse>> {
         return marvelServiceImpl.getCharactersByEventId(eventId)
     }
 
