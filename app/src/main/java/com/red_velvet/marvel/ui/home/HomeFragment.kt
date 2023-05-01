0498d5b7a9f7187968a678c41826495b7438b555
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-01 16:22:30 +0300

    add example for request

--- app/src/main/java/com/red_velvet/marvel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/home/HomeFragment.kt
@@ -1,7 +1,12 @@
 package com.red_velvet.marvel.ui.home
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.red_velvet.marvel.R
+import com.red_velvet.marvel.data.remote.RetrofitClient
+import com.red_velvet.marvel.data.repository.MarvelRepositoryImpl
 import com.red_velvet.marvel.databinding.FragmentHomeBinding
 import com.red_velvet.marvel.ui.base.BaseFragment
 
@@ -10,4 +15,21 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val layoutIdFragment = R.layout.fragment_home
 
     override val viewModel: HomeViewModel by viewModels()
+
+    val marvelRepositoryImpl = MarvelRepositoryImpl(RetrofitClient.apiService)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.setOnClickListener {
+
+            marvelRepositoryImpl.getCharactersByEventId(116).subscribe({
+                println("TAG: success: $it")
+                Log.i("TAG", "success: $it")
+            }, {
+                Log.i("TAG", "error: $it")
+            })
+        }
+
+
+    }
 }
+
