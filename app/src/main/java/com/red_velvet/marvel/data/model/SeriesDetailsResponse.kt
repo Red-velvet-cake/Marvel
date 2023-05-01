0ae29efa269d460ce1731e5e0db4f8e0113717aa
ay0o5h <aayosh553@gmail.com>
2023-05-01 20:09:56 +0300

    reformat

--- app/src/main/java/com/red_velvet/marvel/data/model/SeriesDetailsResponse.kt
+++ app/src/main/java/com/red_velvet/marvel/data/model/SeriesDetailsResponse.kt
@@ -1,7 +1,5 @@
 package com.red_velvet.marvel.data.model
-
 import com.google.gson.annotations.SerializedName
-
 data class SeriesDetailsResponse(
     @SerializedName("id" )
     var id : Int? = null,
