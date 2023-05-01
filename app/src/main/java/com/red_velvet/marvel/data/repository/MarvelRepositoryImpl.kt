093c7abedba4dd09ca0e18ccfb2d5806cc6dc661
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-01 16:33:04 +0300

    add GetCreatorDetailsResponse.kt

--- app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepositoryImpl.kt
+++ app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.red_velvet.marvel.data.repository
 
 import com.red_velvet.marvel.data.model.GetComicsResponse
+import com.red_velvet.marvel.data.model.GetCreatorDetailsResponse
 import com.red_velvet.marvel.data.remote.MarvelService
 import com.red_velvet.marvel.data.remote.RetrofitClient
 import io.reactivex.rxjava3.core.Single
