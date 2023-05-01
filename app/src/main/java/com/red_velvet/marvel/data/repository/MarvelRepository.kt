23ef65a67fbabca4c5624eb12a02dc56933cf8a9
hawraamahmoud <hawraaalobaidy2000@gmail.com>
2023-05-01 22:26:52 +0300

    Add Service Repository Function

--- app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepository.kt
+++ app/src/main/java/com/red_velvet/marvel/data/repository/MarvelRepository.kt
@@ -3,6 +3,7 @@ package com.red_velvet.marvel.data.repository
 import com.red_velvet.marvel.data.model.ComicsResponse
 import com.red_velvet.marvel.data.model.BaseResponse
 import com.red_velvet.marvel.data.model.CharactersByEventIdResponse
+import com.red_velvet.marvel.data.model.charsbycomicid.CharsByComicIdResponse
 import io.reactivex.rxjava3.core.Single
 
 interface MarvelRepository {
@@ -16,7 +17,9 @@ interface MarvelRepository {
 
 
     //TODO Comic details(Comic chars by comic id)
-
+    fun getCharsByComicId(
+        comicId:Int
+    ):Single<BaseResponse<CharsByComicIdResponse>>
 
     //TODO Comics by Char id
 
