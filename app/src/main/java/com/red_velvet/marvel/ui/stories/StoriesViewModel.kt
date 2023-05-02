bb24888302240b97297eea94b78c39053ee8aa4b
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-02 19:27:59 +0300

    add functions on success and on error

--- app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesViewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesViewModel.kt
@@ -1,6 +1,33 @@
 package com.red_velvet.marvel.ui.stories
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.red_velvet.marvel.data.model.BaseResponse
+import com.red_velvet.marvel.data.model.StoryResponse
+import com.red_velvet.marvel.data.remote.RetrofitClient
+import com.red_velvet.marvel.data.repository.MarvelRepositoryImpl
+import com.red_velvet.marvel.data.util.State
+import com.red_velvet.marvel.ui.base.BaseViewModel
 
-class StoriesViewModel : ViewModel() {
+class StoriesViewModel : BaseViewModel() {
+    val repository = MarvelRepositoryImpl(RetrofitClient.apiService)
+    private val _Stories: MutableLiveData<State<List<StoryResponse>>> = MutableLiveData()
+    val stories: LiveData<State<List<StoryResponse>>> get() = _Stories
+
+    init {
+        getStories()
+    }
+
+    private fun getStories() {
+
+
+    }
+
+    private fun onGetStoriesError(error: Throwable) {
+        _Stories.postValue(State.Failed(error.message.toString()))
+    }
+
+    private fun onGetStoriesSuccess(response: BaseResponse<StoryResponse>) {
+        _Stories.postValue(State.Success(response.data?.results!!))
+    }
 }
