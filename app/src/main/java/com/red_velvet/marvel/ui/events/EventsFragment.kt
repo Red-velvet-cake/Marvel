554f25b06ccf39cde31bc21e1cc535317c0a5aef
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-05-04 19:23:14 +0300

    fix EventsFragment.kt

--- app/src/main/java/com/red_velvet/marvel/ui/events/EventsFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/events/EventsFragment.kt
@@ -4,6 +4,7 @@ package com.red_velvet.marvel.ui.events
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
 import com.red_velvet.marvel.R
 import com.red_velvet.marvel.databinding.FragmentEventsBinding
 import com.red_velvet.marvel.ui.base.BaseFragment
@@ -12,10 +13,9 @@ import com.red_velvet.marvel.ui.base.BaseViewModel
 
 class EventsFragment() : BaseFragment<FragmentEventsBinding>() {
     override val layoutIdFragment: Int = R.layout.fragment_events
-    override val viewModel: BaseViewModel = EventsViewModel()
+    override val viewModel:EventsViewModel by viewModels()
     override fun setUp() {
-        val eventsViewModel = viewModel as EventsViewModel
-        val adapter = EventsAdapter(emptyList(), eventsViewModel)
+        val adapter = EventsAdapter(emptyList(), viewModel)
         binding.recyclerViewEvents.adapter = adapter
 
         binding.editTextSearch.addTextChangedListener(object : TextWatcher {
@@ -23,11 +23,11 @@ class EventsFragment() : BaseFragment<FragmentEventsBinding>() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                eventsViewModel.getAllEvents(s.toString())
+                viewModel.getAllEvents(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
-                eventsViewModel.getAllEvents()
+                viewModel.getAllEvents()
             }
         })
 
