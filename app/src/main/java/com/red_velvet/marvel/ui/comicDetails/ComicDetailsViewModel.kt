40de1751d837890c094d28fa5d449f06d821ccfe
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-04 16:24:27 +0300

    complete view model

--- app/src/main/java/com/red_velvet/marvel/ui/comicDetails/ComicDetailsViewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/comicDetails/ComicDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package com.red_velvet.marvel.ui.comicDetails
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.red_velvet.marvel.data.model.*
 import com.red_velvet.marvel.data.remote.RetrofitClient
@@ -7,112 +8,34 @@ import com.red_velvet.marvel.data.repository.MarvelRepositoryImpl
 import com.red_velvet.marvel.data.util.State
 import com.red_velvet.marvel.ui.MarvelRepository
 import com.red_velvet.marvel.ui.base.BaseViewModel
+import com.red_velvet.marvel.ui.base.LiveDataObservablePair
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 
 class ComicDetailsViewModel : BaseViewModel() {
-    private val comicId = 82967
+    private val comicId = 1308
 
-
-    private val _comicsDetails: MutableLiveData<State<ComicsResponse>> = MutableLiveData()
-    val comicsDetails: MutableLiveData<State<ComicsResponse>> get() = _comicsDetails
+    private val _comicsDetails: MutableLiveData<State<List<ComicsResponse>>> = MutableLiveData()
+    val comicsDetails: LiveData<State<List<ComicsResponse>>> get() = _comicsDetails
 
     private val _creators: MutableLiveData<State<List<CreatorsResponse>>> = MutableLiveData()
-    val creators: MutableLiveData<State<List<CreatorsResponse>>> get() = _creators
+    val creators: LiveData<State<List<CreatorsResponse>>> get() = _creators
 
-    private val _characters: MutableLiveData<State<List<Characters>>> = MutableLiveData()
-    val characters: MutableLiveData<State<List<Characters>>> get() = _characters
-
+    private val _characters: MutableLiveData<State<List<CharactersResponse>>> = MutableLiveData()
+    val characters: LiveData<State<List<CharactersResponse>>> get() = _characters
 
     private val repository: MarvelRepository = MarvelRepositoryImpl(RetrofitClient.apiService)
 
     init {
-        getComicsDetails(comicId)
-        getCreators(comicId)
-        getCharacters()
+        fetchData(comicId)
     }
 
-    private fun getComicsDetails(comicId: Int) {
-
-        _comicsDetails.postValue(State.Loading)
-
-        val disposable = repository.getComicDetail(comicId)
-            .doOnError {
-                onGetComicDetailsError(it)
-            }
-            .doOnSuccess {
-                onGetComicDetailsSuccess(it)
-            }
-            .subscribeBy(
-                onError = ::onGetComicDetailsError, onSuccess = ::onGetComicDetailsSuccess
-            )
-
-        disposable.addTo(compositeDisposable)
-
+    private fun fetchData(comicId: Int) {
+        return getDataForMultipleRequests(
+            LiveDataObservablePair(_comicsDetails, repository.getComicDetail(comicId)),
+            LiveDataObservablePair(_creators, repository.getComicCreatorByComicId(comicId)),
+            LiveDataObservablePair(_characters, repository.getCharsByComicId(comicId))
+        )
     }
-
-    private fun getCreators(comicId: Int) {
-        _creators.postValue(State.Loading)
-
-        val disposable = repository.getComicCreatorByComicId(comicId)
-            .doOnError {
-                onGetCreatorsError(it)
-            }
-            .doOnSuccess {
-                onGetCreatorsSuccess(it)
-            }
-            .subscribeBy(
-                onError = ::onGetCreatorsError, onSuccess = ::onGetCreatorsSuccess
-            )
-
-        disposable.addTo(compositeDisposable)
-
-    }
-
-    private fun getCharacters() {
-        _creators.postValue(State.Loading)
-
-        // TODO: get characters by comic id not all characters
-        val disposable = repository.getCharacters()
-            .doOnError {
-                onGetCharactersError(it)
-            }
-            .doOnSuccess {
-                onGetCharactersSuccess(it)
-            }
-            .subscribeBy(
-                onError = ::onGetCharactersError, onSuccess = ::onGetCharactersSuccess
-            )
-
-        disposable.addTo(compositeDisposable)
-
-
-    }
-
-
-    private fun onGetComicDetailsError(error: Throwable) {
-        _comicsDetails.postValue(State.Failed(error.message.toString()))
-    }
-
-    private fun onGetCreatorsError(error: Throwable) {
-        _creators.postValue(State.Failed(error.message.toString()))
-    }
-
-    private fun onGetCharactersError(error: Throwable) {
-        _characters.postValue(State.Failed(error.message.toString()))
-    }
-
-    private fun onGetComicDetailsSuccess(response: BaseResponse<ComicsResponse>) {
-        _comicsDetails.postValue(State.Success(response.data?.results?.first()!!))
-    }
-
-    private fun onGetCreatorsSuccess(response: BaseResponse<CreatorsResponse>) {
-        _creators.postValue(State.Success(response.data?.results!!))
-    }
-
-    private fun onGetCharactersSuccess(response: BaseResponse<Characters>) {
-        _characters.postValue(State.Success(response.data?.results!!))
-    }
-
 
 }
