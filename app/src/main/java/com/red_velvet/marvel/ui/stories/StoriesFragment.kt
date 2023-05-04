9ca79e8df32f178dceb7c7d618f0f6fcc6be4eea
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-04 17:52:48 +0300

    add StoriesFragment.kt

---
+++ app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesFragment.kt
@@ -0,0 +1,16 @@
+package com.red_velvet.marvel.ui.stories
+
+import com.red_velvet.marvel.databinding.FragmentStoriesBinding
+import com.red_velvet.marvel.ui.base.BaseFragment
+import com.red_velvet.marvel.ui.base.BaseViewModel
+
+class StoriesFragment : BaseFragment<FragmentStoriesBinding>() {
+    override val layoutIdFragment: Int
+        get() = TODO("Not yet implemented")
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+}
