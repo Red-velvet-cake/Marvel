55e7a8177784311f00f7100af9ffe3b468d2ec13
Ethaar hussien <ethaar7735@gmail.com>
2023-05-04 18:14:22 +0300

    use base response

--- app/src/main/java/com/red_velvet/marvel/ui/series/SeriesViewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/series/SeriesViewModel.kt
@@ -2,19 +2,17 @@ package com.red_velvet.marvel.ui.series
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.red_velvet.marvel.data.model.BaseResponse
 import com.red_velvet.marvel.data.model.SeriesResponse
 import com.red_velvet.marvel.data.remote.RetrofitClient
 import com.red_velvet.marvel.data.repository.MarvelRepositoryImpl
 import com.red_velvet.marvel.data.util.State
 import com.red_velvet.marvel.ui.MarvelRepository
 import com.red_velvet.marvel.ui.base.BaseViewModel
-import io.reactivex.rxjava3.kotlin.addTo
-import io.reactivex.rxjava3.kotlin.subscribeBy
+
 
 class SeriesViewModel : BaseViewModel() {
-    private val _series: MutableLiveData<State<List<SeriesResponse>>> = MutableLiveData()
 
+    private val _series: MutableLiveData<State<List<SeriesResponse>>> = MutableLiveData()
     val series: LiveData<State<List<SeriesResponse>>> get() = _series
 
     private val repository: MarvelRepository = MarvelRepositoryImpl(RetrofitClient.apiService)
@@ -24,26 +22,7 @@ class SeriesViewModel : BaseViewModel() {
     }
 
     private fun getSeries() {
-        _series.postValue(State.Loading)
-        try {
-            repository.getAllSeries()
-                .doOnError(::onGetSeriesError)
-                .doOnSuccess(::onGetSeriesSuccess)
-                .subscribeBy(
-                    onError = ::onGetSeriesError,
-                    onSuccess = ::onGetSeriesSuccess
-                ).addTo(compositeDisposable)
-        } catch (e: Exception) {
-            onGetSeriesError(e)
-        }
-
+       getData(_series,repository.getAllSeries())
     }
 
-    private fun onGetSeriesError(error: Throwable) {
-        _series.postValue(State.Failed(error.message.toString()))
-    }
-
-    private fun onGetSeriesSuccess(response: BaseResponse<SeriesResponse>) {
-        _series.postValue(State.Success(response.data?.results!!))
-    }
 }
