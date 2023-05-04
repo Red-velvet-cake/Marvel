86706433b665c7bd32d5fe3892d3cc6ee4b7d1f8
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-05-04 17:16:37 +0300

    complete ui

--- app/src/main/java/com/red_velvet/marvel/data/util/BindingAdapters.kt
+++ app/src/main/java/com/red_velvet/marvel/data/util/BindingAdapters.kt
@@ -3,8 +3,10 @@ package com.red_velvet.marvel.data.util
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.red_velvet.marvel.data.model.Thumbnail
+import com.red_velvet.marvel.ui.base.BaseAdapter
 
 @BindingAdapter(value = ["app:showWhenLoading"])
 fun <T> showWhenLoading(view: View, state: State<T>?){
@@ -34,6 +36,15 @@ fun <T> showWhenSuccess(view: View, state: State<T>?){
 }
 
 @BindingAdapter(value = ["app:imageUrl"])
-fun loadImage(view:ImageView,thumbnail: Thumbnail){
-    Glide.with(view).load(thumbnail.toUrl()).into(view)
+fun loadImage(view:ImageView,thumbnail: Thumbnail?){
+    Glide.with(view).load(thumbnail?.toUrl()).into(view)
+}
+
+@BindingAdapter(value = ["app:setItems"])
+fun <T> setRecyclerItems(view: RecyclerView, items:List<T>?){
+    if (items !=null){
+        (view.adapter as BaseAdapter<T>).setItems(items)
+    }else{
+        (view.adapter as BaseAdapter<T> ).setItems(emptyList())
+    }
 }
