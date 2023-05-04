de554e716629f85b4b92efe1a93aa7c3e11ca97c
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-04 17:59:44 +0300

    implement StoriesFragment.kt functions

--- app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesFragment.kt
@@ -1,16 +1,17 @@
 package com.red_velvet.marvel.ui.stories
 
+import androidx.fragment.app.viewModels
+import com.red_velvet.marvel.R
 import com.red_velvet.marvel.databinding.FragmentStoriesBinding
 import com.red_velvet.marvel.ui.base.BaseFragment
-import com.red_velvet.marvel.ui.base.BaseViewModel
 
 class StoriesFragment : BaseFragment<FragmentStoriesBinding>() {
-    override val layoutIdFragment: Int
-        get() = TODO("Not yet implemented")
-    override val viewModel: BaseViewModel
-        get() = TODO("Not yet implemented")
+    override val layoutIdFragment = R.layout.fragment_stories
+    override val viewModel: StoriesViewModel by viewModels()
 
     override fun setUp() {
-        TODO("Not yet implemented")
+        binding.root.setOnClickListener {
+            viewModel.getStories()
+        }
     }
 }
--- app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesViewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesViewModel.kt
@@ -18,7 +18,7 @@ class StoriesViewModel : BaseViewModel() {
         getStories()
     }
 
-    private fun getStories() {
-        getData(_stories, repository.getStories())
-    }
+     fun getStories() {
+         getData(_stories, repository.getStories())
+     }
 }
