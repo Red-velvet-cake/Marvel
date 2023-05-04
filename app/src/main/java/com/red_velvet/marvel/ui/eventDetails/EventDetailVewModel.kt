37a05940c18f2935ca9e9000c2128f000d6f784e
ay0o5h <aayosh553@gmail.com>
2023-05-04 09:56:37 +0300

    view model

--- app/src/main/java/com/red_velvet/marvel/ui/eventDetails/EventDetailVewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/eventDetails/EventDetailVewModel.kt
@@ -1,51 +1,37 @@
 package com.red_velvet.marvel.ui.eventDetails
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.red_velvet.marvel.data.model.BaseResponse
+import com.red_velvet.marvel.data.model.CharactersResponse
+import com.red_velvet.marvel.data.model.CreatorsResponse
 import com.red_velvet.marvel.data.model.EventsResponse
 import com.red_velvet.marvel.data.remote.RetrofitClient
 import com.red_velvet.marvel.data.repository.MarvelRepositoryImpl
 import com.red_velvet.marvel.data.util.State
 import com.red_velvet.marvel.ui.base.BaseViewModel
-import io.reactivex.rxjava3.kotlin.addTo
-import io.reactivex.rxjava3.kotlin.subscribeBy
-import java.lang.Exception
-
 
 class EventDetailVewModel: BaseViewModel() {
+    private  val eventId=116
     private val repository=MarvelRepositoryImpl(RetrofitClient.apiService)
-    private var _event=MutableLiveData<State<EventsResponse>>()
-    val event:MutableLiveData<State<EventsResponse>>
+    private var _event:MutableLiveData<State<List<EventsResponse>>> = MutableLiveData()
+    val event:MutableLiveData<State<List<EventsResponse>>>
         get()=_event
+    private var _characters:MutableLiveData<State<List<CharactersResponse>>> =   MutableLiveData()
+    val characters:MutableLiveData<State<List<CharactersResponse>>>
+        get()= _characters
+    private var _creators:MutableLiveData<State<List<CreatorsResponse>>>  = MutableLiveData()
+    val creators:MutableLiveData<State<List<CreatorsResponse>>>
+        get()=_creators
 
     init {
         getEvent()
     }
-    private fun getEvent(){
-        _event.postValue(State.Loading)
-        try {
-            repository.getEventDetails(1)
-                .doOnError (
-                    ::onError
-                )
-                .doOnSuccess (
-                    ::onSuccess
-                ).subscribeBy(
-                    onError = ::onError,
-                    onSuccess=::onSuccess
-                )
-                .addTo(compositeDisposable)
-
-        }catch (e:Exception){
-            onError(e)
-        }
+     fun getEvent() {
+        getData(_event,repository.getEventDetails(eventId))
     }
-    private fun onError(error: Throwable){
-        _event.postValue(State.Failed(error.message.toString()))
+     fun getCreatorsEventId() {
+        getData(_creators,repository.getCreatorsByEventId(eventId))
     }
-    private fun onSuccess(response: BaseResponse<EventsResponse>){
-        _event.postValue(State.Success(response.data?.results!!.))
+     fun getCharactersEventId() {
+        getData(_characters,repository.getCharactersByEventId(eventId))
     }
-
 }
--- app/src/main/java/com/red_velvet/marvel/ui/eventDetails/EventDetailsFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/eventDetails/EventDetailsFragment.kt
@@ -1,14 +1,66 @@
 package com.red_velvet.marvel.ui.eventDetails
 
+import android.opengl.Visibility
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.red_velvet.marvel.R
 import com.red_velvet.marvel.databinding.FragmentEventDetailsBinding
 import com.red_velvet.marvel.ui.base.BaseFragment
+import com.red_velvet.marvel.data.util.State
+
 
 class EventDetailsFragment:BaseFragment<FragmentEventDetailsBinding>() {
     override val layoutIdFragment=R.layout.fragment_event_details
     override val viewModel:EventDetailVewModel by viewModels()
     override fun setUp() {
-        TODO("Not yet implemented")
+        viewModel.getEvent()
+        viewModel.getCharactersEventId()
+        viewModel.getCreatorsEventId()
+
+
+        viewModel.event.observe(this){
+            when(it){
+                is State.Failed -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Success -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Loading -> {
+                    binding.pBar.visibility= View.VISIBLE
+                }
+
+            }
+        }
+        viewModel.creators.observe(this){
+            when(it){
+                is State.Failed -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Success -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Loading -> {
+                    binding.pBar.visibility= View.VISIBLE
+                }
+
+            }
+        }
+        viewModel.characters.observe(this){
+            when(it){
+                is State.Failed -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Success -> {
+                    binding.pBar.visibility= View.GONE
+                }
+                is State.Loading -> {
+                    binding.pBar.visibility= View.VISIBLE
+                }
+
+            }
+        }
+
     }
+
 }
