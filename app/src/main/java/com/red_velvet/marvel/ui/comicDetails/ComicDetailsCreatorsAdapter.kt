807dc9343d5a5401b7a46bfdf25f33e5e718ecbd
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-04 17:52:27 +0300

    create creator adapter

---
+++ app/src/main/java/com/red_velvet/marvel/ui/comicDetails/ComicDetailsCreatorsAdapter.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.marvel.ui.comicDetails
+
+import com.red_velvet.marvel.R
+import com.red_velvet.marvel.data.model.CreatorsResponse
+import com.red_velvet.marvel.ui.base.BaseAdapter
+
+class ComicDetailsCreatorsAdapter(
+    items: List<CreatorsResponse>,
+    listener: ComicDetailsCreatorListenerInteraction,
+) : BaseAdapter<CreatorsResponse>(items, listener) {
+    override val layoutId: Int = R.layout.comic_item_creator
+}
