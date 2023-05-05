b7bc1e99804c2018371968d11bbf22059160ff1d
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-05 17:01:30 +0300

    change list type for comic adapter

--- app/src/main/java/com/red_velvet/marvel/ui/comics/ComicAdapter.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/comics/ComicAdapter.kt
@@ -1,12 +1,13 @@
 package com.red_velvet.marvel.ui.comics
 
 import com.red_velvet.marvel.R
+import com.red_velvet.marvel.data.model.ComicsResponse
 import com.red_velvet.marvel.ui.base.BaseAdapter
 
-class CollectionAdapter(
-    items: List<ComicsCollection>,
+class ComicAdapter(
+    items: List<ComicsResponse>,
     listener: ComicsInteractionListener
-) : BaseAdapter<ComicsCollection>(items, listener) {
+) : BaseAdapter<ComicsResponse>(items, listener) {
 
     override val layoutId = R.layout.item_comic
 }
