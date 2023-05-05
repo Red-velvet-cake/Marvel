f6c17c2d1aafd61fe8609d9e6b5b061bf57c3bfe
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-05 20:13:18 +0300

    make comics nested recycler extent base adapter

--- app/src/main/java/com/red_velvet/marvel/ui/comics/ComicsScreenAdapter.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/comics/ComicsScreenAdapter.kt
@@ -1,49 +1,24 @@
 package com.red_velvet.marvel.ui.comics
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.red_velvet.marvel.R
 import com.red_velvet.marvel.databinding.ListComicsBinding
+import com.red_velvet.marvel.ui.base.BaseAdapter
 
 class ComicsScreenAdapter(
-    private var items: List<ComicsCollection>,
-    private val comicsListener: ComicsInteractionListener
-) : RecyclerView.Adapter<ComicsScreenAdapter.ItemViewHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ItemViewHolder {
-        return ItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.list_comics,
-                parent,
-                false
-            )
-        )
-    }
+    items: List<ComicsCollection>,
+    private val listener: ComicsInteractionListener,
+) : BaseAdapter<ComicsCollection>(items, listener) {
+    override val layoutId = R.layout.list_comics
 
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val currentItem = items[position]
-
-        holder.binding.apply {
-            textViewTitle.text = currentItem.title
-            recyclerComics.adapter = ComicAdapter(
-                currentItem.comics.toData() ?: emptyList(),
-                comicsListener
-            )
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        super.onBindViewHolder(holder, position)
+        when (holder) {
+            is ItemViewHolder -> {
+                (holder.binding as ListComicsBinding).recyclerComics.adapter = ComicAdapter(
+                    emptyList(),
+                    listener
+                )
+            }
         }
     }
-
-    override fun getItemCount() = items.size
-
-    fun setItems(newItems: List<ComicsCollection>) {
-        items = newItems
-    }
-
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val binding = ListComicsBinding.bind(itemView)
-    }
-
 }
