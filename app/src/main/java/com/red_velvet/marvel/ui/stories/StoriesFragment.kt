03c05c80cff062dace7cb11060993913bfd0e378
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-05 14:17:11 +0300

    enhance on StoriesFragment.kt

--- app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/stories/StoriesFragment.kt
@@ -1,5 +1,7 @@
 package com.red_velvet.marvel.ui.stories
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.red_velvet.marvel.R
 import com.red_velvet.marvel.databinding.FragmentStoriesBinding
@@ -8,10 +10,12 @@ import com.red_velvet.marvel.ui.base.BaseFragment
 class StoriesFragment : BaseFragment<FragmentStoriesBinding>() {
     override val layoutIdFragment = R.layout.fragment_stories
     override val viewModel: StoriesViewModel by viewModels()
-
-    override fun setUp() {
-        binding.root.setOnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
             viewModel.getStories()
-        }
+        val adapter = StoriesAdapter(emptyList(),viewModel)
+        binding.storiesRecyclerView.adapter=adapter
+    }
+    override fun setUp() {
     }
 }
