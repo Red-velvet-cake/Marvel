8ce1191a033e9d0ed0ac9623988db3151afe1a68
AndrewAboalhana <hana_hany6@yahoo.com>
2023-05-05 13:00:02 +0300

    enhancement on the view model

--- app/src/main/java/com/red_velvet/marvel/ui/serieDetails/SeriesDetailsFragment.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/serieDetails/SeriesDetailsFragment.kt
@@ -1,17 +1,14 @@
 package com.red_velvet.marvel.ui.serieDetails
 
-
 import androidx.fragment.app.viewModels
 import com.red_velvet.marvel.R
-import com.red_velvet.marvel.databinding.FragmentSerieDetailsBinding
+import com.red_velvet.marvel.databinding.FragmentSeriesDetailsBinding
 import com.red_velvet.marvel.ui.base.BaseFragment
 import com.red_velvet.marvel.ui.serieDetails.adapter.CreatorsAdapter
-import com.red_velvet.marvel.ui.serieDetails.viewModel.SerieDetailsViewModel
 
+class SeriesDetailsFragment : BaseFragment<FragmentSeriesDetailsBinding>() {
 
-class SerieDetails : BaseFragment<FragmentSerieDetailsBinding>() {
-
-    override val layoutIdFragment = R.layout.fragment_serie_details
+    override val layoutIdFragment = R.layout.fragment_series_details
 
     override val viewModel: SerieDetailsViewModel by viewModels()
     override fun setUp() {
