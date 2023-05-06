0b39cc77af52ea1657eea3f9b6d7052c6f5fa529
ay0o5h <aayosh553@gmail.com>
2023-05-06 10:55:33 +0300

    add text view

--- app/src/main/java/com/red_velvet/marvel/ui/eventDetails/adapter/CharactersAdapter.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/eventDetails/adapter/CharactersAdapter.kt
@@ -8,5 +8,5 @@ import com.red_velvet.marvel.ui.base.BaseInteractionListener
 class CharactersAdapter (items:List<CharactersResponse>,
 
 ): BaseAdapter<CharactersResponse>(items,) {
-    override val layoutId: Int = R.layout.item_creators_event
+    override val layoutId: Int = R.layout.item_charactors
 }
