64964cf319510da1b6a265cce3e7ee4dd9db98c0
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-06 12:58:15 +0300

    uddate viewmodel based on the new changes

--- app/src/main/java/com/red_velvet/marvel/ui/comicDetails/ComicDetailsViewModel.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/comicDetails/ComicDetailsViewModel.kt
@@ -28,11 +28,9 @@ class ComicDetailsViewModel : BaseViewModel(), ComicDetailsCreatorListenerIntera
     private val repository: MarvelRepository = MarvelRepositoryImpl(RetrofitClient.apiService)
 
     fun fetchData(comicId: Int) {
-//        return getDataForMultipleRequests(
-//            LiveDataObservablePair(_comicsDetails, repository.getComicDetail(comicId)),
-//            LiveDataObservablePair(_creators, repository.getComicCreatorByComicId(comicId)),
-//            LiveDataObservablePair(_characters, repository.getCharsByComicId(comicId))
-//        )
+        fetchComicDetailsData(comicId)
+        fetchCreatorsByComicIDData(comicId)
+        fetchCharactersByComicIDData(comicId)
     }
 
     override fun onClickCharacter(creator: CreatorsResponse) {
@@ -42,5 +40,65 @@ class ComicDetailsViewModel : BaseViewModel(), ComicDetailsCreatorListenerIntera
     override fun onClickCreator(creator: CreatorsResponse) {
         TODO("Not yet implemented")
     }
+
+
+    private fun fetchComicDetailsData(comicId: Int) {
+        bindStateUpdates(
+            repository.getComicDetail(comicId),
+            onNext = ::onGetComicDetailsDataSuccess,
+            onError = ::onGetComicDetailsDataError
+        )
+    }
+
+
+    private fun fetchCreatorsByComicIDData(comicId: Int) {
+        bindStateUpdates(
+            repository.getComicCreatorByComicId(comicId),
+            onNext = ::onGetCreatorsByComicIDDataSuccess,
+            onError = ::onGetCreatorsByComicIDDataError
+        )
+    }
+
+
+    private fun fetchCharactersByComicIDData(comicId: Int) {
+        bindStateUpdates(
+            repository.getCharsByComicId(comicId),
+            onNext = ::onGetCharactersByComicIDDataSuccess,
+            onError = ::onGetCharactersByComicIDDataError
+        )
+    }
+
+
+    private fun onGetComicDetailsDataSuccess(state: State<List<ComicsResponse>?>) {
+        _comicsDetails.postValue(State.Loading)
+        state.toData()?.let {
+            _comicsDetails.postValue(State.Success(it))
+        }
+    }
+
+    private fun onGetComicDetailsDataError(e: Throwable) {
+        _comicsDetails.postValue(State.Failed(e.message.toString()))
+    }
+
+    private fun onGetCreatorsByComicIDDataSuccess(state: State<List<CreatorsResponse>?>) {
+        state.toData()?.let {
+            _creators.postValue(State.Success(it))
+        }
+    }
+
+    private fun onGetCreatorsByComicIDDataError(e: Throwable) {
+        _creators.postValue(State.Failed(e.message.toString()))
+    }
+
+    private fun onGetCharactersByComicIDDataSuccess(state: State<List<CharactersResponse>?>) {
+        state.toData()?.let {
+            _characters.postValue(State.Success(it))
+        }
+    }
+
+    private fun onGetCharactersByComicIDDataError(e: Throwable) {
+        _characters.postValue(State.Failed(e.message.toString()))
+    }
+
 
 }
