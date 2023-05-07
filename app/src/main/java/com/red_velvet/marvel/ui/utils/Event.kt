fd402b4df50819650a5d8d74432621a59ec3bfc1
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-05-07 22:53:19 +0300

    add single live data event class

---
+++ app/src/main/java/com/red_velvet/marvel/ui/utils/Event.kt
@@ -0,0 +1,26 @@
+package com.red_velvet.marvel.ui.utils
+
+import androidx.lifecycle.Observer
+
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
+
+class EventObserve<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>) {
+        event.getContentIfNotHandled()?.let {
+            onEventUnhandledContent(it)
+        }
+    }
+}
