a788a137967eceb942b0da2e06b394812353f614
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-05-09 00:50:56 +0300

    hide bottom navigation in details

--- app/src/main/java/com/red_velvet/marvel/ui/utils/extensions.kt
+++ app/src/main/java/com/red_velvet/marvel/ui/utils/extensions.kt
@@ -1,27 +1,14 @@
 package com.red_velvet.marvel.ui.utils
 
-import androidx.activity.addCallback
-import androidx.fragment.app.Fragment
+import android.view.View
 import com.red_velvet.marvel.data.model.Thumbnail
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
-
-fun <T : Any> Single<T>.applySchedulers(): Single<T> {
-    return this.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-}
-
-fun <T : Any> Observable<T>.applySchedulers(): Observable<T> {
-    return this.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-}
 
 fun Thumbnail.toUrl() = this.path + "." + this.extension
 
-fun Fragment.onBackPressed(function: () -> Unit) {
-    requireActivity().onBackPressedDispatcher.addCallback(requireActivity()) {
-        function()
-    }
+fun View.hideView() {
+    visibility = View.GONE
+}
+
+fun View.showView() {
+    visibility = View.VISIBLE
 }
