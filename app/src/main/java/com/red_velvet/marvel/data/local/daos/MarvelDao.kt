69bc810bd49e4ba7af9dd8875782e3c32f657cc4
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-20 13:46:28 +0300

    create search query dao functions

--- app/src/main/java/com/red_velvet/marvel/data/local/daos/MarvelDao.kt
+++ app/src/main/java/com/red_velvet/marvel/data/local/daos/MarvelDao.kt
@@ -7,6 +7,7 @@ import androidx.room.Query
 import com.red_velvet.marvel.data.local.entity.CharacterEntity
 import com.red_velvet.marvel.data.local.entity.ComicEntity
 import com.red_velvet.marvel.data.local.entity.EventEntity
+import com.red_velvet.marvel.data.local.entity.SearchQueryEntity
 import io.reactivex.rxjava3.core.Observable
 
 @Dao
@@ -29,4 +30,14 @@ interface MarvelDao {
 
     @Query("SELECT * FROM EVENTS")
     fun getAllEvents(): Observable<List<EventEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSearchQuery(searchQuery: SearchQueryEntity)
+
+    @Query("SELECT * FROM SEARCH_QUERIES ORDER BY timestamp DESC LIMIT 5")
+    fun getAllSearchQueries(): Observable<List<SearchQueryEntity>>
+
+    @Query("DELETE FROM SEARCH_QUERIES WHERE id = :id")
+    fun deleteSearchQuery(id: Int)
+
 }
