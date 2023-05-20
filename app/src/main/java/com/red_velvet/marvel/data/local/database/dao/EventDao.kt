c9cbf5b61e8460a26b8834622e0ce3bacf363547
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-20 04:50:34 +0300

    edit event dao

--- app/src/main/java/com/red_velvet/marvel/data/local/database/dao/EventDao.kt
+++ app/src/main/java/com/red_velvet/marvel/data/local/database/dao/EventDao.kt
@@ -1,19 +1,13 @@
 package com.red_velvet.marvel.data.local.database.dao
 
-import android.database.Observable
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Query
 import com.red_velvet.marvel.data.local.database.entity.EventEntity
 import io.reactivex.rxjava3.core.Completable
 
 @Dao
 interface EventDao {
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertEvent(event: EventEntity): Completable
-
-    @Query("SELECT * FROM EVENT_TABLE")
-    fun getAllEvents(): Observable<List<EventEntity>>
 }
